ab166b92194a13f5b48c52a9dcff33d831b58d96
ranggahakim05 <ranggahakim05@gmail.com>
2024-06-27 03:02:03 +0700

    minor update
    update font and add face in win/lose popup

--- EXPO-FIT_Mei-2024-master/Assets/Scripts/WinLose.cs
+++ EXPO-FIT_Mei-2024-master/Assets/Scripts/WinLose.cs
@@ -7,6 +7,11 @@ public class WinLose : MonoBehaviour
     public Image player1ResultImage; // Text to display Player 1's result (Winner/Draw/Lose)
     public Image player2ResultImage; // Text to display Player 2's result (Winner/Draw/Lose)
 
+    public Image rubahJantan, rubahBetina;
+
+    public Sprite jantanAngry, jantanHappy;
+    public Sprite betinaAngry, betinaHappy;
+
     public Sprite win, lose;
 
     // Function to set the result text for Player 1
@@ -16,12 +21,14 @@ public class WinLose : MonoBehaviour
         {
             case GameResult.Winner:
                 player1ResultImage.sprite = win;
+                rubahJantan.sprite = jantanHappy;
                 break;
             case GameResult.Draw:
                 
                 break;
             case GameResult.Lose:
                 player1ResultImage.sprite = lose;
+                rubahJantan.sprite= jantanAngry;
                 break;
             default:
                 break;
@@ -35,12 +42,14 @@ public class WinLose : MonoBehaviour
         {
             case GameResult.Winner:
                 player2ResultImage.sprite = win;
+                rubahBetina.sprite = betinaHappy;
                 break;
             case GameResult.Draw:
                 
                 break;
             case GameResult.Lose:
                 player2ResultImage.sprite = lose;
+                rubahBetina.sprite = betinaAngry;
                 break;
             default:
                 break;
